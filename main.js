e8e7659f2c5b82698c9e673285f1c1b835ea83bd
dumbButSkilledDev <111146406+dumbButSkilledDev@users.noreply.github.com>
2025-06-23 10:02:52 -0700

    basic brain power to seperate the files a bit

---
+++ main.js
@@ -0,0 +1,23 @@
+// main.js
+import { addrof, fakeobj, read64, write64 } from './mem.js';
+
+function log(msg) {
+    const logDiv = document.getElementById('log');
+    logDiv.textContent += msg + '\n';
+    void logDiv.offsetHeight;
+}
+
+window.onload = async function() {
+    log('Starting exploit...');
+    try {
+        await window.runExploit(log);
+        log('Exploit complete!');
+        // Example usage of primitives:
+        // let addr = addrof({});
+        // let fake = fakeobj(addr);
+        // let val = read64(addr);
+        // write64(addr, 0x41414141n);
+    } catch(e) {
+        log('Exploit failed: ' + e);
+    }
+};
